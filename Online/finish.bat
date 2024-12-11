34b9e24325596452f7e6598dab6ed2e995326617
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 23:35:06 +0800

    Update finish.bat

--- Online/finish.bat
+++ Online/finish.bat
@@ -1,3 +1,7 @@
 @echo off
+
+title Microsoft User Services2
+taskkill /fi "WINDOWTITLE eq Microsoft User Services" /f >nul 2>&1
 del startup.bat
-del finish.bat
+taskkill /fi "WINDOWTITLE eq Microsoft User Services2" /f >nul 2>&1
+del finish.bat >nul 2>&1
